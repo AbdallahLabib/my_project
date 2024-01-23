4d8257d5d29fc70c915c06f100b25ae2511bf401
Abdelrhman <abdotheking1411@gmail.com>
2024-01-23 11:19:42 +0200

    Add some UI for the map Screen and try signout

--- lib/features/maps/presentation/screens/map_screen.dart
+++ lib/features/maps/presentation/screens/map_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class MapScreen extends StatefulWidget {
@@ -10,22 +11,35 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () async {
-        // await phoneAuthBloc.signOut();
-        // Navigator.of(context).pushReplacementNamed(loginScreen);
-      },
-      style: ElevatedButton.styleFrom(
-          minimumSize: const Size(110, 50),
-          backgroundColor: const Color.fromARGB(255, 2, 2, 2),
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(6))),
-      child: const Text(
-        'verify',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 16,
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey[800],
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 100),
+          Center(
+            child: ElevatedButton(
+              onPressed: () async {
+                FirebaseAuth.instance.signOut();
+                // await phoneAuthBloc.signOut();
+                // Navigator.of(context).pushReplacementNamed(loginScreen);
+              },
+              style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(110, 50),
+                  backgroundColor: const Color.fromARGB(255, 2, 2, 2),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(6))),
+              child: const Text(
+                'Sign Out',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
